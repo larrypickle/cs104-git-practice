0b61b7d1148b531d83131c1a444b35f6df67502b
Larry Wu <larrywu@usc.edu>
2020-08-20 00:51:31 -0700

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 // main() is where program execution begins.
 int main() {
-   cout << "Hello World"; // prints Hello World
+   cout << "les gooo"; // prints Hello World
    return 0;
 }
